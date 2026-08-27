6e7694431cc0824f3008ef1cfd91c4d7b7fb9d03
Pedro Henrique Brás <pedrohenriquebras329@gmail.com>
2026-08-27 15:42:41 -0300

    Add functionality to print even and odd numbers

--- QST1.py
+++ QST1.py
@@ -1,1 +1,9 @@
-
+num = int(input("Digite um número: "))
+print(f"Números pares até {num}:")
+for c in range(1, num):
+    if c % 2 == 0:
+        print(c, end=' ')
+print(f"\nNúmeros impares até {num}:")
+for c in range(1, num):
+    if c % 2 == 1:
+        print(c, end=' ')
